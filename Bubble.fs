3599cd7ad450c2cfec99ed1caf0b3e653166c8e7
Aurametrix <irene@aurametrix.com>
2016-04-30 13:12:28 -0700

    delete

--- Bubble.fs
+++
@@ -1,9 +0,0 @@
-let BubbleSort (lst : list<int>) = 
-    let rec sort accum rev lst =
-        match lst, rev with
-        | [], true -> accum |> List.rev
-        | [], false -> accum |> List.rev |> sort [] true
-        | x::y::tail, _ when x > y -> sort (y::accum) false (x::tail)
-        | head::tail, _ -> sort (head::accum) rev tail
-    sort [] true lst
- 
